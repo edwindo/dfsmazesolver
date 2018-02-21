1b5f6f0799a53d382b971ad1adab158bf9efe399
mike-hale <michael.hale48@gmail.com>
2018-02-20 18:14:08 -0800

    client/server test v1.0

--- client.c
+++ client.c
@@ -4,3 +4,20 @@
 /* Date:    02/18/2018          */
 /* File:    client.c            */
 /********************************/
+
+#include <stdio.h>
+#include "sel_repeat.h"
+
+int main(int argc, int argv[]) {
+  int port;
+  if (argc != 2) {
+    fprintf(stderr, "Erorr\n");
+    exit(1);
+  }
+  else
+    port = atoi(argv[1]);
+
+  int meta_i = connect_rdt(port, "127.0.0.1");
+  write_sr(meta_i, "THIS IS A TEST", 15);
+  mark_done(meta_i);
+}
--- server.c
+++ server.c
@@ -4,3 +4,25 @@
 /* Date:    02/18/2018          */
 /* File:    server.c            */
 /********************************/
+
+#include <stdio.h>
+#include "sel_repeat.h"
+
+int main(int argc, int argv[]) {
+  int port;
+  if (argc != 2) {
+    fprintf(stderr, "Erorr\n");
+    exit(1);
+  }
+  else
+    port = atoi(argv[1]);
+  
+  int meta_i = init_serv(port, "");
+  char buf[256];
+  int nbyte;
+  while((nbyte = read_sr(meta_i, buf, 256)) >= 0) {
+    write(1, buf, nbyte);
+  }
+  
+  printf("\nEnd of transmission\n");
+}
