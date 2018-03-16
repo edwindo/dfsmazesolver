e09be6470ab00cacb8efc6fcdc96cb9a787461fd
mike-hale <michael.hale48@gmail.com>
2018-03-16 01:33:56 -0700

    Sending packets correctly, not copying data correctly

--- client.c
+++ client.c
@@ -8,15 +8,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "sel_repeat.h"
 
+void handle_sig(int arg);
+
 int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    printf("One command line argument: Filename\n");
+    exit(1);
+  }
   init_summary();
+  signal(SIGINT, handle_sig);
+  int fd = open(argv[1], O_RDONLY);
   int meta_i = connect_rdt("127.0.0.1");
-  char* test = "THIS IS A TWO HUNDRED AND FIFTY SIX CHARACTER STRING FOR TESTING OUR PROJECT REGARDING RELIABLE DATA TRANSFER IN A LOSSY NETWORK SETTING. UNFORTUNATELY I UNDERESTIMATED HOW MANY CHARACTERS TWO HUNDRED AND FIFTY SIX IS, SO NOW THIS IS JUST RAMBLING TO FILL";
-  for (int i = 0; i < 10; i++)
-    write_sr(meta_i, test, 256);
+  char buf[256];
+  int nchars;
+  while ((nchars = read(fd, buf, 256)) > 0)
+    write_sr(meta_i, buf, 256);
   mark_done(meta_i);
   finish_sr();
   print_summary();
+}
+
+void handle_sig(int arg)
+{
+  print_summary();
+  exit(1);
 }
